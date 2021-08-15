11c97b94b2ecb9080115abf3f41b71269ff0b1c6
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-08-15 13:44:18 +0430

    003 The last tween method _ gsap.fromTo()

--- 1. The basics of GSAP/app.js
+++ 1. The basics of GSAP/app.js
@@ -1,12 +1,11 @@
 const img1 = document.querySelector(".img1");
 
 document.querySelector(".btn").addEventListener("click", () => {
-  gsap.from(img1, {
-    autoAlpha: 0,
-    rotation: 45,
-    scale: 0.5,
-    x: 100,
-    y: -100,
-    duration: 2,
-  });
+  gsap.fromTo(
+    img1,
+    {
+      autoAlpha: 0,
+    },
+    {autoAlpha: 1, duration: 2}
+  );
 });
