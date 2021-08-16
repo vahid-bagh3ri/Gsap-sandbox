a13a71e36e21609bf9fd6007c36d264c69c10546
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-08-16 14:05:07 +0430

    007 nimate random values

--- 1. The basics of GSAP/app.js
+++ 1. The basics of GSAP/app.js
@@ -1,16 +1,9 @@
 const img1 = document.querySelector(".img1");
 
-gsap.from("img", {
-  autoAlpha: 0,
-  y: -100,
-  ease: "power4",
-  duration: 2,
+gsap.to(".btn", {
   delay: 0.8,
-  stagger: (index) => {
-    console.log(index);
-  },
-  // stagger: {
-  //   each: 0.5,
-  //   from: "end",
-  // },
+  duration: 0.4,
+  stagger: 0.2,
+  x: "random([20,50,80,-90,-50])",
+  // x: "random(-100,100,10)",
 });
