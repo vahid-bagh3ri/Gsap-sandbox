97e8a19515623569a8e867932aca45d4540f5eb8
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-08-16 15:49:45 +0430

    012 he utility methods of the tweens

--- 1. The basics of GSAP/app.js
+++ 1. The basics of GSAP/app.js
@@ -1,3 +1,19 @@
 const img1 = document.querySelector(".img1");
 
-gsap.set(img1,{opacity:0})
+const tween = gsap.to(".img2", {
+  y: 200,
+  paused: true,
+  duration: 5,
+  // paused: false,
+});
+
+// tween.kill();
+// tween.delay(1)
+// tween.duration(5);
+
+setTimeout(() => {
+  // tween.resume();
+
+  tween.seek(2.5);
+  tween.play();
+}, 2000);
