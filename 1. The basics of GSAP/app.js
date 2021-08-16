79ffe0c6bd49e0478cd9704af7ddb0a80e54d185
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-08-16 15:42:56 +0430

    011 et some values:

--- 1. The basics of GSAP/app.js
+++ 1. The basics of GSAP/app.js
@@ -1,16 +1,3 @@
 const img1 = document.querySelector(".img1");
 
-gsap.registerEffect({
-  name: "imgAnimation",
-  effect: (targets, config) => {
-    return gsap.to(targets, {
-      duration: config.duration,
-      y: 200,
-      scale: 1.4,
-      rotation: 360,
-    });
-  },
-  defaults: {duration: 2},
-});
-
-gsap.effects.imgAnimation(img1, {duration: 5});
+gsap.set(img1,{opacity:0})
