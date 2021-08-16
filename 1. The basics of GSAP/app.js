deb0f863fcbdd8fa7f9dfc3988617daf60b78316
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-08-16 14:42:04 +0430

    009 rigger some event callbacks

--- 1. The basics of GSAP/app.js
+++ 1. The basics of GSAP/app.js
@@ -1,9 +1,11 @@
 const img1 = document.querySelector(".img1");
 
 gsap.to(img1, {
-  keyframes: [
-    {duration: 0.3, x: 100},
-    {duration: 0.3, y: 100},
-    {duration: 0.3, x: 200},
-  ],
+  x: 100,
+  duration: 1,
+  repeat: 1,
+  onComplete: () => console.log("COMPLETE"),
+  onStart: () => console.log("START"),
+  onUpdate: () => console.log("UPDATE"),
+  onRepeat: () => console.log("REPEAT"),
 });
