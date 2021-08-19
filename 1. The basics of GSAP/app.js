8bcd053a935ab6a9564946f51e2709f2f6bf2f0f
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-08-19 17:29:19 +0430

    003 Properties and methods of the timeline

--- 1. The basics of GSAP/app.js
+++ 1. The basics of GSAP/app.js
@@ -1,7 +1,25 @@
-const TL = gsap.timeline();
+const TL = gsap.timeline({
+  defaults: {
+    duration: 1,
+    ease: "power4.out",
+  },
+  // repeat: -1, // infinite loop
+  // yoyo: true,
+  // paused: true,
+  // onComplete: () => console.log("complete"),
+  // onStart: () => console.log("start"),
+});
 
-TL.from(".img1", {autoAlpha: 0, y: -50, duration: 1});
-TL.from(".img2", {autoAlpha: 0, y: -50, duration: 1}, "-=0.75"); // relative to it's parent
-TL.from(".img3", {autoAlpha: 0, y: -50, duration: 1}, 3); // absolute
-TL.from("h1", {autoAlpha: 0, y: -50, duration: 1}, "<"); // cascade simultaneously with it's parent
-TL.from("p", {autoAlpha: 0, y: -50, duration: 1}, ">"); // default behavior cascade after his parent
+TL.from(".img1", {autoAlpha: 0, y: -50});
+TL.from(".img2", {autoAlpha: 0, y: -50}, "-=0.75"); // relative to it's parent
+TL.from(".img3", {autoAlpha: 0, y: -50}, 3); // absolute
+TL.from("h1", {autoAlpha: 0, y: -50}, "<"); // cascade simultaneously with it's parent
+TL.from("p", {autoAlpha: 0, y: -50, ease: "power4.out"}, ">"); // default behavior cascade after his parent
+
+// setTimeout(() => {
+//   TL.play();
+// }, 1500);
+
+setTimeout(() => {
+  TL.restart();
+}, 7000);
