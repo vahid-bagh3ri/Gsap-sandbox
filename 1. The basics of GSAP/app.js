bb8aa2b06365f2c7f3914d65c0a547e2f7ef75c6
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-08-19 17:20:26 +0430

    002 The position parameters

--- 1. The basics of GSAP/app.js
+++ 1. The basics of GSAP/app.js
@@ -1,7 +1,7 @@
 const TL = gsap.timeline();
 
 TL.from(".img1", {autoAlpha: 0, y: -50, duration: 1});
-TL.from(".img2", {autoAlpha: 0, y: -50, duration: 1});
-TL.from(".img3", {autoAlpha: 0, y: -50, duration: 1});
-TL.from("h1", {autoAlpha: 0, y: -50, duration: 1});
-TL.from("p", {autoAlpha: 0, y: -50, duration: 1});
+TL.from(".img2", {autoAlpha: 0, y: -50, duration: 1}, "-=0.75"); // relative to it's parent
+TL.from(".img3", {autoAlpha: 0, y: -50, duration: 1}, 3); // absolute
+TL.from("h1", {autoAlpha: 0, y: -50, duration: 1}, "<"); // cascade simultaneously with it's parent
+TL.from("p", {autoAlpha: 0, y: -50, duration: 1}, ">"); // default behavior cascade after his parent
