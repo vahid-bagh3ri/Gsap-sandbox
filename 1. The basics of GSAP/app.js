041a17f144803ebef7d623ce8ec59a950bc57076
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-08-19 17:50:25 +0430

    004 The add() and addLabel() handy methods

--- 1. The basics of GSAP/app.js
+++ 1. The basics of GSAP/app.js
@@ -6,20 +6,25 @@ const TL = gsap.timeline({
   // repeat: -1, // infinite loop
   // yoyo: true,
   // paused: true,
-  // onComplete: () => console.log("complete"),
-  // onStart: () => console.log("start"),
+  onComplete: () => console.log("complete"),
+  onStart: () => console.log("start"),
 });
 
-TL.from(".img1", {autoAlpha: 0, y: -50});
-TL.from(".img2", {autoAlpha: 0, y: -50}, "-=0.75"); // relative to it's parent
-TL.from(".img3", {autoAlpha: 0, y: -50}, 3); // absolute
-TL.from("h1", {autoAlpha: 0, y: -50}, "<"); // cascade simultaneously with it's parent
-TL.from("p", {autoAlpha: 0, y: -50, ease: "power4.out"}, ">"); // default behavior cascade after his parent
+TL.from(".img1", {autoAlpha: 0, y: -50})
+  .from(".img2", {autoAlpha: 0, y: -50}, "-=0.75") // relative to it's parent
+  .from(".img3", {autoAlpha: 0, y: -50}, 3) // absolute
+  .from("h1", {autoAlpha: 0, y: -50}, "<") // cascade simultaneously with it's parent
+  // .addLabel("endAnim", "-=2")
+  .add(() => {
+    console.log("End of the timeline");
+  })
+  .from("p", {autoAlpha: 0, y: -50, ease: "power4.out"}, ">"); // default behavior cascade after his parent
 
 // setTimeout(() => {
 //   TL.play();
 // }, 1500);
 
-setTimeout(() => {
-  TL.restart();
-}, 7000);
+// setTimeout(() => {
+//   TL.seek("endAnim");
+//   TL.play();
+// }, 1000);
